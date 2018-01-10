d3eda242809197116d728b2302b9b756e8bff829
 WibeTeam <wibeteam@WibeTeams-Mac-mini.local>
2018-01-10 14:43:49 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass',function(){
+	return gulp.src(['sass/**/*.sass','sass/**/*.scss'])
+		.pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))
+		.pipe(gulp.dest('css'))
+});
+gulp.task('watch',function(){
+	return gulp.watch(['sass/**/*.sass','sass/**/*.scss'],['sass']);
+});
+
+gulp.task('default',['watch']);
